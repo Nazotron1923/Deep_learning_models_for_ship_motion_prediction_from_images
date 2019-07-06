df753179d8cbbdc6f378b0f58066d40b5bde5e72
Nazotron1923 <kaminskyi.nazar.1923@gmail.com>
2019-07-06 18:45:41 +0200

    add model CNN_PR_FC

--- Pre/models.py
+++ Pre/models.py
@@ -585,3 +585,78 @@ class CNN_LSTM_decoder_images_PR(nn.Module):
         decoder_output = decoder_output.view(image_s[0].size(0), predict_n_pr, -1)
 
         return decoder_output, encoder_hidden, decoder_hidden
+
+
+class CNN_PR_FC (nn.Module):
+    def __init__(self, cnn_fc_size = 1024, num_output=20, h_dim=2688, z_dim=1024):
+        super(CNN_PR_FC, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size= 3, stride=2, padding=1),
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding = 1),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding = 1),
+        )
+
+        self.mu = nn.Linear(h_dim, z_dim)
+        self.std = nn.Linear(h_dim, z_dim)
+
+        self.dropout0 = nn.Dropout(p=0.3)
+        self.fc1 = nn.Linear(5376  , 1024) #5376 / 20736
+
+        self.fc2 = nn.Linear(cnn_fc_size, int(cnn_fc_size/2)) #5376 / 20736
+        self.fc22 = nn.Linear(int(cnn_fc_size/2), 1024)
+        self.fc3 = nn.Linear(1024, 128)
+        self.fc4 = nn.Linear(128, num_output)
+        self.dropout1 = nn.Dropout(p=0.3)
+
+        self.dropout2 = nn.Dropout(p=0.4)
+        self.dropout3 = nn.Dropout(p= 0.4)
+
+    def reparameterize(self, mu, logvar, cuda):
+        std = logvar.mul(0.5).exp_()
+        if cuda:
+            esp = torch.randn(*mu.size()).cuda()
+        else:
+            esp = torch.randn(*mu.size())
+        z = mu + std * esp
+        return z
+
+    def bottleneck(self, h, cuda):
+        mu, logvar = self.mu(h), F.relu(self.std(h))
+        z = self.reparameterize(mu, logvar, cuda)
+        return z
+
+    def encode(self, x, cuda):
+        h = self.encoder(x)
+        h = h.view(h.size(0), -1)
+        z = self.bottleneck(h, cuda)
+        return z
+
+
+    def forward(self, x, p_and_roll, num_images, cuda):
+
+        features = [self.encode(x[:,i,:,:,:], cuda) for i in range(num_images-1, -1, -1)]
+
+        PR = [p_and_roll[:,i,:] for i in range(num_images-1, -1, -1)]
+
+        input_fc = [ torch.cat((features[i], PR[i]), 1).view(x.size(0), 1, -1) for i in range(num_images)]
+        input_fc = torch.cat(input_fc, 2).view(x.size(0), 1, -1)
+
+        x = F.relu(self.fc2(input_fc))
+        x = self.dropout2(x)
+        x = F.relu(self.fc22(x))
+        x = self.dropout2(x)
+
+        x = F.relu(self.fc3(x))
+        x = self.dropout3(x)
+        x = torch.tanh(self.fc4(x)).view(x.size(0), -1, 2)
+
+        return x
--- Pre/train_tot.py
+++ Pre/train_tot.py
@@ -23,7 +23,7 @@ from Pre.utils import loadLabels, gen_dict_for_json, write_result
 
 from Pre.utils import JsonDataset_universal as JsonDataset
 
-from Pre.models import ConvolutionalNetwork_p1, ConvolutionalNetwork_p2, CNN_stack_PR_FC, CNN_LSTM_encoder_decoder_images_PR, AutoEncoder, LSTM_encoder_decoder_PR, CNN_LSTM_encoder_decoder_images, CNN_LSTM_decoder_images_PR
+from Pre.models import ConvolutionalNetwork_p1, ConvolutionalNetwork_p2, CNN_stack_PR_FC, CNN_LSTM_encoder_decoder_images_PR, AutoEncoder, LSTM_encoder_decoder_PR, CNN_LSTM_encoder_decoder_images, CNN_LSTM_decoder_images_PR, CNN_PR_FC
 
 """if above line didn't work, use following two lines instead"""
 import matplotlib.pyplot as plt
@@ -243,6 +243,8 @@ def main(train_folder, num_epochs = 50, batchsize = 32,
     print("modeltype----", model_type)
     if model_type == "CNN_stack_PR_FC":
         model = CNN_stack_PR_FC(num_channel=use_n_channels, cnn_fc_size = 1024 + use_n_im*2, num_output=predict_n_pr*2 )
+    elif model_type == "CNN_PR_FC":
+        model = CNN_PR_FC(cnn_fc_size = use_n_im*1026, num_output=predict_n_pr*2)
     elif model_type == "CNN_LSTM_encoder_decoder_images_PR":
         model = CNN_LSTM_encoder_decoder_images_PR(h_dim=2688, z_dim=1024, encoder_input_size = use_n_im*1026, encoder_hidden_size = 1024, decoder_hidden_size = 1024,  output_size = 2*predict_n_pr)
         #pretrained model
@@ -528,7 +530,7 @@ if __name__ == '__main__':
     parser.add_argument('-bs', '--batchsize', help='Batch size', default= 16, type=int)
     parser.add_argument('--seed', help='Random Seed', default=42, type=int)
     parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training')
-    parser.add_argument('--model_type', help='Model type: cnn', default="CNN_LSTM_decoder_images_PR", type=str, choices=['CNN_LSTM_encoder_decoder_images', 'LSTM_encoder_decoder_PR', 'CNN_stack_PR_FC', 'CNN_LSTM_encoder_decoder_images_PR', 'CNN_LSTM_decoder_images_PR'])
+    parser.add_argument('--model_type', help='Model type: cnn', default="CNN_PR_FC", type=str, choices=['CNN_PR_FC', 'CNN_LSTM_encoder_decoder_images', 'LSTM_encoder_decoder_PR', 'CNN_stack_PR_FC', 'CNN_LSTM_encoder_decoder_images_PR', 'CNN_LSTM_decoder_images_PR'])
     parser.add_argument('-lr', '--learning_rate', help='Learning rate', default=1e-5, type=float)
     parser.add_argument('-t', '--time_gap', help='Time gap', default= 12, type=int)
     parser.add_argument('-u', '--use_sec', help='How many seconds using for prediction ', default= 10, type=int)
